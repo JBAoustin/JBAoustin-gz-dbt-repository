cf3acc9d3bd54c77b191ac164b899b5967796615
Jean-Baptiste Aoustin <jbaptiste.aoustin@gmail.com>
2025-11-05 13:13:52 +0000

    commit mercredi first

--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
@@ -1,21 +1,18 @@
-with 
-
+with
 source as (
-
     select * from {{ source('gz_raw_data', 'raw_gz_ship') }}
-
 ),
-
 renamed as (
-
     select
         orders_id,
         shipping_fee,
         logcost,
-        CAST(ship_cost AS FLOAT64)
-
+        CAST (ship_cost AS INT64) AS ship_cost
     from source
-
 )
-
 select * from renamed
+
+
+
+
+
